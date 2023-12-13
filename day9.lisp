4031778772089e487681ce02daa2f818d3b0e699
tetrazolium <115483176+tetrazolium@users.noreply.github.com>
2023-12-13 03:51:49 -0600

    Finished part one for day 9

--- day9.lisp
+++ day9.lisp
@@ -5,24 +5,29 @@
 (defun predict ()
   (let ((delta 0) (degree nil) (fileexpr (mapcar #'(lambda (x)
               (read-from-string (format nil "(~a)" x))) (uiop:read-file-lines "day9example.txt"))))
-    (loop for x in fileexpr do
-          (print x)
-          (print (test2 x)))
+    (loop for x in fileexpr
+          sum (anticipate x))
 ))
 
-(defun test2 (lst)
-  "Recursive function - TCO function to get the degree of a sequence"
-  (let ((base 0))
-    (values (labels ((rec (lst acc)
+(defun anticipate (lst &optional start-at-end)
+  "Recursive function - Attempt to predict the next element of a list, following the form described by day 9"
+    (labels ((rec (lst acc)
 (null lst)
              (cond ((null lst)
                     return-from test2 nil)
                    ((every #'zerop lst)
                     acc)
-                   (t (setf base (first lst))
-                    (rec (map 'list #'- lst (rest lst)) (1+ acc))))))
-    (- (rec lst 0) 1))
-            (- base))))
+                   (t 
+                    (rec (map 'list #'- (rest lst) lst) (+ (car (last lst)) acc))))))
+    (rec lst 0)))
+
+(defmacro generate-recursive-case (funcname backwards)
+  `(t (,funcname (map 'list #'- (rest lst) lst)
+                 (,(if (equalp backwards t)
+                       `(- (first lst))
+                       `(+ (car (last lst)))
+                       )
+                       acc))))
 
 (defun our-length (lst)
   (labels ((rec (lst acc)
@@ -32,6 +37,8 @@
     (rec lst 0)))
 (defun difference (inp)
   (map 'list #'- inp (rest inp)))
+
+
 (defun pyramid-num (r n) ;formula straight from wikipedia
   (* (/ (+ (expt n 2) n) 2) ;note that this 
      (/ (- (* n (- r 2)) (- r 5)) 3)))
