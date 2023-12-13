8dcc748aa1c170219c09e0bb6077ed97556cba4b
tetrazolium <115483176+tetrazolium@users.noreply.github.com>
2023-12-13 13:48:00 -0600

    Part 2 of day9

--- day9.lisp
+++ day9.lisp
@@ -2,24 +2,25 @@
   "See paip"
   ((apply #'append (mapcar fn lst))))
 (* (+ 3 4) (+ 5 6))
-(defun predict ()
+(defun predict-and-sum ()
   (let ((delta 0) (degree nil) (fileexpr (mapcar #'(lambda (x)
-              (read-from-string (format nil "(~a)" x))) (uiop:read-file-lines "day9example.txt"))))
+              (read-from-string (format nil "(~a)" x))) (uiop:read-file-lines "day9.txt"))))
     (loop for x in fileexpr
           sum (anticipate x))
 ))
 
 (defun anticipate (lst &optional start-at-end)
-  "Recursive function - Attempt to predict the next element of a list, following the form described by day 9"
-    (labels ((rec (lst acc)
+  "Recursive function - Attempt to predict the previous element of a list, following the form described by day 9
+   I would make it general but I'm bad at macros"
+    (labels ((rec (lst acc multiplier)
 (null lst)
              (cond ((null lst)
                     return-from test2 nil)
                    ((every #'zerop lst)
                     acc)
                    (t 
-                    (rec (map 'list #'- (rest lst) lst) (+ (car (last lst)) acc))))))
-    (rec lst 0)))
+                    (rec (map 'list #'- (rest lst) lst) (+ (* multiplier (first lst)) acc) (- multiplier))))))
+    (rec lst 0 1)))
 
 (defmacro generate-recursive-case (funcname backwards)
   `(t (,funcname (map 'list #'- (rest lst) lst)
